c17422c4dd1aaa39bf8aa7a4e5bac0568a14304a
Çağatay Atabey <atabeycagatay@gmail.com>
2020-12-27 01:53:32 +0300

    V1

---
+++ CheckedList/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CheckedList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(284, 214);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(466, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SelectedItem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(466, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(12, 233);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(284, 23);
+            this.showButton.TabIndex = 3;
+            this.showButton.Text = "Show CheckedItem";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(466, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(342, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "DisplayMember";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(342, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "ValueMember";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(12, 262);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(284, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "ClearButton";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 285);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "CheckedListBox With Db";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ CheckedList/Form1.cs
@@ -0,0 +1,84 @@
+﻿using FirebirdSql.Data.FirebirdClient;//Firebird Liblary
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;//Configuration Liblary
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckedList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FbConnection fbconn = new FbConnection(ConfigurationManager.ConnectionStrings["ConnectionName"].ConnectionString);// Get Connection From AppConfig
+        public static string value = "";
+        public static string display = "";
+        void getCheckListItem()
+        {
+            //YOU MUST CONFIGURE YORU TABLE NAME 
+            fbconn.Close();
+            string sql = "SELECT ID,TEXT FROM TABLE";
+            DataTable dt = new DataTable();
+            FbDataAdapter adp = new FbDataAdapter(sql, fbconn);
+            
+            fbconn.Open();
+            adp.Fill(dt);
+            fbconn.Close();
+            checkedListBox1.DataSource = dt;
+            checkedListBox1.DisplayMember = "TEXT";
+            checkedListBox1.ValueMember = "ID";
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getCheckListItem();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            value = " ";
+            display = " ";
+            int i =0;
+            DataRow r;
+            for ( i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                
+
+                r = ((DataRowView)this.checkedListBox1.CheckedItems[i]).Row;
+                display += (r[this.checkedListBox1.DisplayMember]).ToString();
+                value += (r[this.checkedListBox1.ValueMember]).ToString();
+                r = null;
+               
+                
+                //This if block is add ',' all item and last item don't add
+                if (i != checkedListBox1.CheckedItems.Count - 1)
+                {
+                    value += ",";
+                    display += "," + "\n";
+                }
+                else
+                    value += "";
+                    display += "";
+
+            }
+            label2.Text = value.ToString();
+            label3.Text = display.ToString();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            while (checkedListBox1.CheckedIndices.Count > 0)
+                checkedListBox1.SetItemChecked(checkedListBox1.CheckedIndices[0], false);
+        }
+    }
+}
